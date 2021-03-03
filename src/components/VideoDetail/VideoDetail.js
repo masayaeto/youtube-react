3233a1e6cf002963e4da014502be2a0cee3cd201
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-03-03 22:36:31 +0900

    関連動画取得の効率化

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,6 +1,4 @@
-import React, {useEffect, useContext} from 'react'
-import {useLocation} from 'react-router-dom'
-import { fetchSelectedData } from '../../apis/index'
+import React, {useContext} from 'react'
 import {Store} from '../../store/index'
 import VideoPlay from '../VideoPlay/VideoPlay'
 import Style from './VideoDetail.module.scss'
@@ -8,18 +6,6 @@ import Linkify from 'react-linkify';
 
 const VideoDetail = () => {
     const { globalState, setGlobalState} = useContext(Store)
-    const location = useLocation()
-    const setSelectedVideo = async () => {
-        const searchParams = new URLSearchParams(location.search)
-        const id = searchParams.get('v')
-        await fetchSelectedData(id).then((res) => {
-            const item = res.data.items.shift()
-            setGlobalState({type: 'SET_SELECTED', payload: {selected: item}})
-        })
-    }
-    useEffect(() => {
-        setSelectedVideo()
-    }, [location])
     return globalState.selected && globalState.selected.id ? (
         <div className={Style.wrap}>
             <VideoPlay id={globalState.selected.id}/>
